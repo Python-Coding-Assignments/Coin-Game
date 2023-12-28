e1e2384901a9bbbbf01681f38a45816e78bcea26
Garrett Ellis <154717520+garrettbovo@users.noreply.github.com>
2023-12-28 12:40:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if charInput == "Y" or charInput == "y":
     matchAmount = float(input("What amount of money would you like to match? --> "))
     print("Okay! You need to match the value of $" + str("{:.2f}".format(matchAmount)))
 
+    #getting the user's inputs for number of coins
     print("Enter the number of:")
     numHalves = int(input("\tHalf Dollars: "))
     numQuarters = int(input("\tQuarters: "))
@@ -22,23 +23,33 @@ if charInput == "Y" or charInput == "y":
     numNickels = int(input("\tNickels: "))
     numPennies = int(input("\tPennies: "))
 
+    #conditional statement which runs if one of the user's inputs is less than zero
     if numHalves < 0 or numQuarters < 0 or numDimes < 0 or numNickels < 0 or numPennies < 0:
         print("You cannot enter a negative number of coins. Aborting game.")
 
+    #conditional statement which runs if all of the user's inputs are positive
     else:
+        #calculating the monetary value of all coins that the user input
         totalInput = (numHalves * constant.half) + (numQuarters * constant.quarter) + (numDimes * constant.dime) + (numNickels * constant.nickel) + (numPennies * constant.penny)
 
+        #conditional statement which runs if total input equals the user's match amount
         if totalInput == matchAmount:
             print("You WIN!")
+
+        #conditional statement which runs if total input is greater than the user's match amount    
         elif totalInput > matchAmount:
             print("You LOST by: $" + str("{:.2f}".format((totalInput - matchAmount), 2)))
+
+        #conditional statement which runs if user's match amount is greater than the total input    
         else:
             print("You LOST by: $" + str("{:.2f}".format((matchAmount - totalInput), 2))) 
 
         print("Thanks for playing!")
 
+#conditional statement which runs if user decides to not play the game
 elif charInput == "N" or charInput == "n":
     print("Guess you are scared! Come back when you are ready to play")
 
+#conditional statement which runs if the user enters an input other than Y, y, N, or n
 else:
     print("Invalid entry, aborting program.")
